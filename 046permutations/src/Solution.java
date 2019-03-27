d034e9b54699bbaebee98ede40db8f99710c6268
DownhillSZC <37056376+DownhillSZC@users.noreply.github.com>
2019-03-27 15:30:51 +0800

    Add files via upload

---
+++ 046permutations/src/Solution.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        handle(nums, 0, result, new ArrayList<Integer>());
+        return result;
+    }
+    
+    public void handle(int[] nums, int n, List<List<Integer>> result, List<Integer> temp){
+        if(n == nums.length){
+            result.add(temp);
+            return;
+        }
+        for(int i=0; i<nums.length; i++){
+        	if(temp.contains(nums[i])) continue;
+        	temp.add(nums[i]);
+            handle(nums, n+1, result, temp);
+            temp.remove(nums.length-1);
+        }
+    }
+}
